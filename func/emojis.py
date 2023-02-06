3b6944b78fc5ddfc678ee0ed01e2b89c0721964d
GithubNeon <87894558+GithubNeon@users.noreply.github.com>
2023-02-06 10:51:57 +0900

    Add files via upload

---
+++ func/emojis.py
@@ -0,0 +1,59 @@
+class emojis:
+    def one():return           "<:one:1049630030432575539>"
+    def two():return           "<:two:1049630034274562110>"
+    def three():return         "<:three:1049630032441638912>"
+    def four():return          "<:four:1049630028217983037>"
+    def five():return          "<:five:1049630026351525928>"
+
+    def good():return          "<:socialnetwork:1046718803834306610>"
+    def bad():return           "<:hand:1046718801942687824>"
+    def ban():return           "<:ban:1046718822205378601>"
+    def ok():return            "<:check:1047060078458581012>"
+    def no():return            "<:cross:1046718847622840400>"
+
+    def delete():return        "<:delete:1046718756908445726>"
+    def trash():return         "<:trash:1046718805713367040>"
+    def lock():return          "<:lock:1047429199608164392>"
+
+    def help():return          "<:interrogation:1046718817620987955>"
+
+    def info():return          "<:exclamation:1046718797970669569>"
+    def userinfo():return      "<:addressbook:1046718807697264690>"
+
+    def people():return        "<:people:1028282408715419798>"
+    def group():return         "<:usersalt:1046718845601189938>"
+
+    def idcard():return        "<:idbadge:1046718843386593290>"
+    def verify():return        "<:shieldcheck:1046718792702640169>"
+
+    def channel():return       "<:hastag:1046718841331400804>"
+    def voice():return         "<:volume:1047062092152324146>"
+    def message():return       "<:commentalt:1046718819822997544>"
+    
+    def game():return          "<:gamepad:1046718766844743710>"
+    def dice():return          "<:dicealt:1047060076562747393>"
+
+    def discord():return       "<:discord:1046718786553782292>"
+    def python():return        "<:python:1046718749081870396>"
+    def github():return        "<:github:1046718788378308678>"
+    def developer():return     "<:developer:1028282383465721866>"
+
+    def settings():return      "<:settings:1047063822025891880>"
+    def share():return         "<:share:1046718809458884608>"
+
+    def owner():return         "<:crown:1046718769340370965>"
+    def tag():return           "<:at:1046718778769162280>"
+    
+    def search():return        "<:search:1047063819723223091>"
+    def clean():return         "<:broom:1046718752080801802>"
+
+    def music():return         "<:musicalt:1046718774826520638>"
+    def musicPlay():return     "<:play:1046718815385440286>"
+    def musicPause():return    "<:pause:1047062090222944367>"
+    def musicDisabled():return "<:playpause:1046718755318804480>"
+    def musicStop():return     "<:square:1048426175778791514>"
+    def musicVloumeU():return  "<:volume_up:1047062092152324146>"
+    def musicVloumeD():return  "<:volume_down:1050670001792766002>"
+    def musicRepeat():return   "<:repeat:1050767487735701604>"
+    def musicShuffle():return  "<:shuffle:1053621186132709496>"
+    def musicPlaylist():return "<:playlist:1068732254186119198>"
