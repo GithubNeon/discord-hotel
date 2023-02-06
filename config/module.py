728808de61c50ef8dec1436d778d34e2402d596b
GithubNeon <87894558+GithubNeon@users.noreply.github.com>
2023-02-06 10:50:37 +0900

    Create module.py

---
+++ config/module.py
@@ -0,0 +1,10 @@
+developer_id = 912950179055935489
+
+en_timeList = ["s", "m", "h"]
+ko_timeList = ["초", "분", "시간"]
+
+en_add_or_removeList = ["add", "remove"]
+ko_add_or_removeList = ["추가", "제거"]
+
+en_commandsList = ["Permision commands", "Utility commands", "Music commands", "Dev commands", "Help commands"]
+ko_commandsList = ["관리 명령어", "유틸리티", "노래 명령어", "도움말"]
